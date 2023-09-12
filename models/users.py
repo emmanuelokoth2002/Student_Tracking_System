88cb8760ddf0805a01917ddd61650ed59ebcb103
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-09-13 00:50:22 +0300

    Updates

--- models/users.py
+++ models/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, redirect, render_template, request, jsonify, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from database import Database
 
@@ -7,29 +7,31 @@ db = Database()
 
 @users_bp.route('/')
 def index():
-    #return "<h1>Hello, World!</h1>"
     return render_template("index.html")
 
 @users_bp.route('/register', methods=['POST'])
 def register_user():
     data = request.json
-    userid = data['userid']
-    username = data['username']
-    password_hash = data['password_hash']
-    role = data['role']
+    userid = data.get('userid')
+    username = data.get('username')
+    password = data.get('password')
+    role = data.get('role')
+
+    if not userid or not username or not password:
+        return jsonify({'message': 'Userid Username and password are required'}), 400
 
     # Check if the username already exists
-    query_check_username = "CheckUser"
-    args_check_username = (userid,username,password_hash)
+    query_check_username = "SELECT * FROM users WHERE username = %s"
+    args_check_username = (username,)
     existing_user = db.get_data(query_check_username, args_check_username)
 
     if existing_user:
         return jsonify({'message': 'Username already exists'}), 400
 
     # Insert the new user into the database
-    password_hash = generate_password_hash(password_hash)
-    query_insert_user = "registeruser"
-    args_insert_user = (userid,username, password_hash, role)
+    password_hash = generate_password_hash(password)
+    query_insert_user = "INSERT INTO users (userid, username, password_hash, role) VALUES (%s, %s, %s, %s)"
+    args_insert_user = (userid, username, password_hash, role)
     
     try:
         db.connect()
@@ -40,18 +42,31 @@ def register_user():
         print("Error:", e)
         return jsonify({'error': 'An error occurred while registering the user'}), 500
 
-@users_bp.route('/login', methods=['POST'])
+@users_bp.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.json
-    username = data['username']
-    password = data['password_hash']
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
 
-    # Retrieve the user from the database
-    query_select_user = "users"
-    args_select_user = (username,)
-    user = db.get_data(query_select_user, args_select_user)
+        if not username or not password:
+            return jsonify({'message': 'Username and password are required'}), 400
 
-    if not user or not check_password_hash(user[0]['password_hash'], password):
-        return jsonify({'message': 'Invalid credentials'}), 401
+        # Retrieve the user from the database
+        query_select_user = "SELECT * FROM users WHERE username = %s"
+        args_select_user = (username,)
+        user = db.get_data(query_select_user, args_select_user)
 
-    return jsonify({'message': 'Login successful'}), 200
+        if not user or not check_password_hash(user[0]['password_hash'], password):
+            return jsonify({'message': 'Invalid credentials'}), 401
+
+        # Redirect to the 'dashboard' route upon successful login
+        return redirect(url_for('users.dashboard'))
+
+    # Render the 'login.html' file for GET requests
+    return render_template("login.html")
+
+@users_bp.route('/dashboard')
+def dashboard():
+    # Render the 'dashboard.html' file for the logged-in user
+    return render_template("dashboard.html")
+
